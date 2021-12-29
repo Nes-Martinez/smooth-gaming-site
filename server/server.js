eb46f5555c2267dfd9316d05a3acaa48c5113ce2
Nes-Martinez <nestor.a.martinez@outlook.com>
2021-12-29 11:38:47 -0500

    Added sequelize/DB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const db = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
+
+const syncDB = () => db.sync();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
