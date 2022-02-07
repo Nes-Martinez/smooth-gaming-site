aa659c3740b365b029a7cedde02f60d563bbc96d
Nes-Martinez <nestor.a.martinez@outlook.com>
2022-02-07 16:28:39 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -15,4 +15,5 @@ app.use("/api", require("./routes"));
 //static file-serving middleware
 app.use(express.static(path.join(__dirname, "..", "images")));
 
-app.listen(5000, () => console.log(`Server is running on 5000`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
