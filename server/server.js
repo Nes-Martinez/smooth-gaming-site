7c6d99e6859db74e2a50d64699152791fd1de701
Nes-Martinez <nestor.a.martinez@outlook.com>
2022-02-07 19:55:12 -0500

    Added body parser

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(bodyParser.json());
 app.use(cors());
 
 // auth and api routes
@@ -16,11 +18,11 @@ app.use("/api", require("./routes"));
 //static file-serving middleware
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.get("/", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 app.use(express.static(path.join(__dirname, "..", "images")));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
