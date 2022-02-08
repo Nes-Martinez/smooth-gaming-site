03f20e9bd55a6519e80d8b0cd0d0f5188460eda9
Nes-Martinez <nestor.a.martinez@outlook.com>
2022-02-07 20:39:44 -0500

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -18,8 +18,14 @@ app.use("/api", require("./routes"));
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/*", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
 app.use(express.static(path.join(__dirname, "..", "images")));
